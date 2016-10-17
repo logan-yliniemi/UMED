20b4ef99a3261b08b633abc3c141fd27e2f95153
logan-yliniemi <logan@unr.edu>
2016-10-17 13:30:22 -0700

    Create main file
    Added UMED.cpp

---
+++ UMED.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Logan Yliniemi on 10/17/16.
+// Last Edit by Logan Yliniemi on 10/17/16.
+//  Copyright © 2016 Logan Yliniemi. All rights reserved.
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!\n";
+    return 0;
+}
