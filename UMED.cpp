2258ea147929d5a8a5b16ca0c557ae38554a6d98
Logan Yliniemi <logan@Logans-MacBook-Pro-2.local>
2016-10-20 13:24:44 -0700

    Partial Implementation
    Initialization and partial implementation before creating the Observation class.

--- UMED.cpp
+++ UMED.cpp
@@ -36,7 +36,8 @@ public:
     vector<bool> to_agent_comm_link;
     
     void init();
-    void start_based_on_policy(policy* pPol, int num_agents);
+    void start_based_on_policy(policy& rPol, int num_agents);
+    void move_to_wp(policy& rPol, int wpnum);
 };
 
 class waypoint{
@@ -53,22 +54,38 @@ public:
 class agent{
 public:
     vehicle V;
+    int id;
     vector<policy> policies;
-    double local;
-    double global;
-    double true_difference;
-    double limited_difference;
-    vector<double> obs_distance; /// best observations of each POI
+    vector<double> my_obs_distance; /// best observations of each POI
+    vector<double> others_obs_distance;
+    vector<bool> comms_P2P_available;
+    int active_policy_index;
     
     /// functions to initialize.
     void init(parameters* pPar);
-    void init_obs_distance(); /// TODO
+    void init_obs_distance(parameters* pPar); /// TODO
+    void start_generation();
+    void select_fresh_policy();
+    
+    void observe_poi_distances(environment* pE,parameters* pPar);
+    void establish_comms_links(vector<agent>* pA,parameters* pPar);
+    void exchange_information_P2P(vector<agent>* pA,parameters* pPar);
+    void exchange_information_general(parameters* pPar);
 };
 
 class policy{
 public:
     vector<waypoint> WP;
     void init(parameters* pPar);
+    
+    void start_generation();
+    double local;
+    double global;
+    double true_difference;
+    double limited_difference;
+    
+    int times_selected;
+    bool active;
 };
 
 class environment{
@@ -95,7 +112,7 @@ public:
 
 class parameters{
 public:
-    const int num_agents = 1;
+    const int num_agents = 3;
     const int num_vehicles = num_agents; // 1 vehicle per agent
     const int num_POI = 5*num_agents;
     const int pop_size = 100;
@@ -114,6 +131,12 @@ public:
     const double min_poi_value = 1;
     const double max_poi_value = 100;
     
+    const int STAT_RUNS = 3;
+    const int GENERATIONS = 100;
+    const bool allow_general_comm_link = false;
+    
+    const double P2P_commlink_dist = 3;
+    
     void init();
 };
 ///////////////////// %%%%%%%%%%%%%%%%%% END CLASS DECLARATIONS %%%%%%%%%%%%%%%%%% /////////////////////
@@ -129,12 +152,79 @@ void agent::init(parameters* pPar){
         X.init(pPar);
         policies.push_back(X);
     }
-    
     /// create a vector of waypoints for each policy (taken care of in policy.init())
     
+    /// give each agent a unique ID;
+    static double x;
+    id = x;
+    x++;
 }
-void agent::init_obs_distance(){
+void agent::init_obs_distance(parameters* pPar){
     /// TODO
+    for(int p = 0; p<pPar->num_POI; p++){
+        double DBL_MAX = std::numeric_limits<double>::max();
+        my_obs_distance.push_back(DBL_MAX);
+        others_obs_distance.push_back(DBL_MAX);
+    }
+}
+void agent::start_generation(){
+    for(int p=0; p<policies.size(); p++){
+        policies.at(p).start_generation();
+    }
+    active_policy_index = -1;
+}
+void agent::select_fresh_policy(){
+    /// selects a random policy that has not been selected this generation:
+    /// "times selected" is zero.
+    int rnd = rand()%policies.size();
+    /// "if the policy has already been used, choose the next one".
+    while(policies.at(rnd).times_selected>0){
+        rnd++;
+        /// "if we get to the end, go to the beginning".
+        if(rnd==policies.size()){
+            rnd=0;
+        }
+    }
+    policies.at(rnd).times_selected++; /// rnd policy has been selected.
+    policies.at(rnd).active = 1; /// rnd policy is active.
+    active_policy_index = rnd; /// active policy is rnd.
+}
+void agent::observe_poi_distances(environment* pE, parameters* pPar){
+    for(int p = 0; p<pPar->num_POI; p++){
+        double delx = V.x - pE->POIs.at(p).x;
+        double dely = V.y - pE->POIs.at(p).y;
+        double delz = V.z - pE->POIs.at(p).z;
+        double dist = sqrt(delx * delx + dely * dely + delz * delz);
+        if(dist < my_obs_distance.at(p)){
+            my_obs_distance.at(p) = dist;
+        }
+    }
+}
+void agent::establish_comms_links(vector<agent>* pA, parameters* pPar){
+    for(int a=0; a<pPar->num_agents; a++){
+        if(a == id){
+            comms_P2P_available.push_back(false);
+        }
+        double delx = V.x - pA->at(a).V.x;
+        double dely = V.x - pA->at(a).V.y;
+        double delz = V.x - pA->at(a).V.z;
+        double dist = sqrt(delx * delx + dely * dely + delz * delz);
+        if(dist < pPar->P2P_commlink_dist){
+            comms_P2P_available.push_back(true);
+        }
+        else{
+            comms_P2P_available.push_back(false);
+        }
+    }
+    assert(comms_P2P_available.size() == pPar->num_agents);
+}
+void agent::exchange_information_P2P(vector<agent>* pA, parameters* pPar){
+    /// TODO
+    for(int a=0; a<pPar->num_agents; a++){
+        for(int p = 0; p<pPar->num_POI; p++){
+            
+        }
+    }
 }
 /////// END AGENT FUNCTIONS ///////
 
@@ -153,6 +243,14 @@ void policy::init(parameters* pPar){
     }
     assert(WP.size() == pPar->num_waypoints);
 }
+void policy::start_generation(){
+    local = 0;
+    global = 0;
+    true_difference = 0;
+    limited_difference = 0;
+    times_selected = 0;
+    active = false;
+}
 /////// END Policy FUNCTIONS ///////
 
 /////// BGN VEHICLE FUNCTIONS ///////
@@ -162,17 +260,27 @@ void vehicle::init(){
     id = index;
     index++;
 }
-void vehicle::start_based_on_policy(policy* pPol, int num_agents){
-    x = pPol->WP.at(0).x;
-    y = pPol->WP.at(0).y;
-    z = pPol->WP.at(0).z;
+void vehicle::start_based_on_policy(policy& rPol, int num_agents){
+    x = rPol.WP.at(0).x;
+    y = rPol.WP.at(0).y;
+    z = rPol.WP.at(0).z;
     
     general_comm_link = false;
     
+    to_agent_comm_link.clear();
     for(int agent = 0; agent < num_agents; agent++){
         to_agent_comm_link.push_back(false);
     }
-
+    assert(to_agent_comm_link.size() == num_agents);
+}
+void vehicle::move_to_wp(policy& rPol, int wp){
+    x = rPol.WP.at(wp).x;
+    y = rPol.WP.at(wp).y;
+    z = rPol.WP.at(wp).z;
+    
+    if(z > 0){
+        general_comm_link = true;
+    }
 }
 /////// END VEHICLE FUNCTIONS ///////
 
@@ -270,7 +378,55 @@ void parameters::init(){
 
 ///////////////////// %%%%%%%%%%%%%%%%%% END CLASS FUNCTIONS %%%%%%%%%%%%%%%%%% /////////////////////
 
+void stat_run(vector<agent>*pA,environment* pE,parameters* pPar);
+void single_generation(vector<agent>*pA,environment* pE,parameters* pPar,int gen);
+void single_simulation(vector<agent>*pA,environment* pE,parameters* pPar);
+void advance(vector<agent>*pA,environment* pE,parameters* pPar, int wpnum);
 
+void stat_run(vector<agent>*pA,environment* pE,parameters* pPar, int SR){
+    cout << "STAT RUN\t\t" << SR << endl;
+    for(int gen = 0; gen<pPar->GENERATIONS; gen++){
+        single_generation(pA,pE,pPar,gen);
+    }
+}
+
+void single_generation(vector<agent>*pA,environment* pE,parameters* pPar,int gen){
+    cout << "GENERATION\t" << gen << endl;
+    for(int a=0; a<pPar->num_agents; a++){
+        pA->at(a).start_generation();
+    }
+    for(int sim=0; sim<pPar->pop_size; sim++){
+        /// select a policy for each agent.
+        for(int a=0; a<pPar->num_agents; a++){
+            pA->at(a).select_fresh_policy();
+        }
+        /// simulate based on selected policy.
+        single_simulation(pA, pE, pPar);
+    }
+}
+
+void single_simulation(vector<agent>*pA,environment* pE,parameters* pPar){
+    for(int a=0; a<pPar->num_agents; a++){
+        int dex = pA->at(a).active_policy_index;
+        policy P = pA->at(a).policies.at(dex);
+        pA->at(a).V.start_based_on_policy(P,pPar->num_agents);
+    }
+    /// vehicles at starting position.
+    for(int ts = 0; ts<pPar->num_waypoints; ts++){
+        advance(pA,pE,pPar,ts);
+    }
+}
+
+void advance(vector<agent>*pA,environment* pE,parameters* pPar, int wpnum){
+    for(int a=0; a<pPar->num_agents; a++){
+        int dex = pA->at(a).active_policy_index;
+        policy P = pA->at(a).policies.at(dex);
+        pA->at(a).V.move_to_wp(P,wpnum);
+    }
+    for(int a=0; a<pPar->num_agents; a++){
+        pA->at(a).establish_comms_links(pA,pPar);
+    }
+}
 
 int main() {
     srand((unsigned)time(NULL));
@@ -280,6 +436,7 @@ int main() {
     parameters* pPar = &P;
     
     vector<agent> Team;
+    vector<agent>* pA = &Team;
     for(int a=0; a<pPar->num_agents; a++){
         agent AA;
         AA.init(pPar);
@@ -288,9 +445,14 @@ int main() {
     assert(Team.size() == pPar->num_agents);
     
     environment E;
+    environment* pE = &E;
     E.init(pPar);
     
+    //// Assume team, environment have been initialized 10/19/16
     
+    for(int i=0; i<pPar->STAT_RUNS; i++){
+        stat_run(pA,pE,pPar,i);
+    }
     
     cout << "End of Program" << endl;
     return 0;
